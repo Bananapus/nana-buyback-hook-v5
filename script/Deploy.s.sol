3ccd973705e111f450a6751ce098013b5b8b160c
mejango <mejango@protonmail.com>
2023-12-18 22:43:14 -0500

    deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -12,12 +12,14 @@ contract Deploy is Script {
         address wethAddress;
         address factoryAddress;
 
+           // Ethereum Mainnet
         if (chainId == 1) {
             chain = "1";
             wethAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
             factoryAddress = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
-        } else if (chainId == 1337) {
-            chain = "1337";
+           // Ethereum Sepolia
+        } else if (chainId == 11_155_111) {
+            chain = "11155111";
             wethAddress = 0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9;
             factoryAddress = 0x0227628f3F023bb0B980b67D528571c95c6DaC1c;
         } else {
