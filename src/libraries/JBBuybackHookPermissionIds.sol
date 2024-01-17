da78ca493b88d6b656c7942ef4f3088fa49a03d7
Filip <f@filip.world>
2024-01-17 11:01:18 -0500

    Update permission IDs

--- src/libraries/JBBuybackHookPermissionIds.sol
+++ src/libraries/JBBuybackHookPermissionIds.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/// @notice Permission IDs for use in `JBPermissions`, for the buyback hook.
+/// @notice Permission IDs for `JBPermissions`. These grant permissions scoped to the `JBBuybackHook`.
 library JBBuybackHookPermissionIds {
-    // [0..18] - JBPermissionIds
-    // 19 - JBOperations2 (ENS/Handle)
-    // 20 - JBUriOperations (Set token URI)
-    // [21..23] - JB721Operations
+    // 1-20 - `JBPermissionIds`
+    // 21 - `JBHandlePermissionIds`
+    // 22-24 - `JB721PermissionIds`
 
-    uint256 public constant SET_POOL_PARAMS = 24;
-    uint256 public constant CHANGE_POOL = 25;
+    uint256 public constant SET_POOL_PARAMS = 25;
+    uint256 public constant CHANGE_POOL = 26;
 }
